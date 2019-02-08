ecc1155229c303139e282f7820d81047ba3e84ae
barema4 <samrubarema6@gmail.com>
2019-02-08 14:26:48 +0300

    chore(test environment): setup environment for writing tests
    - configure jest
    - add travis.yml
    
    [Finishes #163810648]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component} from "react";
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import "./App.css";
-import './App.scss';
 import Landing from './components/layout/Landing';
-
+import'./App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
