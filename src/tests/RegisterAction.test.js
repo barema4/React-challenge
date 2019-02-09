3d92162073b59b387a21697cecfe40f6d1139c3c
barema4 <samrubarema6@gmail.com>
2019-02-09 16:56:34 +0300

    ft(login user): login user to acess service
    -create component for login
    - add redux to the component
    
    [Finishes #163843025]

---
+++ src/tests/RegisterAction.test.js
@@ -0,0 +1,34 @@
+import { registerUser } from '../actions/AuthActions';
+import fetchMock from 'fetch-mock';
+import Enzyme from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import thunk from 'redux-thunk';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+const API_HOST_URL = process.env.API_URL;
+
+it('test comment  action', () => {
+  const url = `${API_HOST_URL}/v2/auth/signup`;
+
+  fetchMock.postOnce(url, 
+    {"user_name": "davidrff" ,
+    "email":"daccvbb@gmail.com" ,
+    "password":"123456789"}
+  );
+  const expectedActions = 
+    [{"payload": 
+    {"email": "daccvbb@gmail.com",
+     "password": "123456789",
+      "user_name": "davidrff"},
+       "type": "GET_ERRORS"}]
+  ;
+  const store = mockStore();
+
+  return store
+    .dispatch( registerUser('userData'))
+    .then(() => expect(store.getActions()).toEqual(expectedActions));
+});
